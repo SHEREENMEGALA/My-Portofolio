3d6e953c0b06310e828009c92e59a0ff4ad3adce
SHEREENMEGALA <shereenmegala421@gmail.com>
2025-09-13 23:21:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,9 @@ window.onscroll = function () {
     header.classList.remove("active");
   }
 };
+
+let nav_links = document.getElementById("links");
+
+function open_close_Menu() {
+  nav_links.classList.toggle("active");
+}
